d53f135df496ea5bbdf82a19f61bfaaed3f124ce
Cole Pazoid <34737903+ColePZ@users.noreply.github.com>
2018-03-18 14:02:20 +0000

    Added the 2 Player Version of Game
    Added the 2 Player Version of Game

--- Ruby Programs/game2player.rb
+++ Ruby Programs/game2player.rb
@@ -4,20 +4,26 @@ puts "#{island}"
 puts "A game by HiddenCale"
 puts "WARNING: DO NOT CLOSE THE COMMAND PROMPT AS THERE IS NO SAVE"
 
-puts "Whats your name?"
-player = gets.chomp
+puts "Player 1, Whats your name?"
+player1 = gets.chomp
 
-puts "Welcome #{player}"
+puts "Player 2, Whats your name?"
+player2 = gets.chomp
+
+puts "Welcome #{player1} and #{player2}"
 puts "You were once on a boat with your fellow pirates"
 puts "Your boat crashed somehow into a rock"
 puts "There is an island near to you."
 puts "Your friends are drowning and you escape"
 puts "You found an island! Its called #{island}"
 
-puts "You found a spider! What do you call your new friend!?"
-spider = gets.chomp
+puts "Player 1! You found a spider! What do you call your new friend!?"
+spiderplay1 = gets.chomp
 
-puts "Your spider, #{spider} . Points towards a cave!"
+puts "Player 2! You found a spider! What do you call your new friend!?"
+spiderplay2 = gets.chomp
+
+puts "Player 1, Your spider, #{spiderplay1} . Points towards a cave!"
 puts "You go into the cave"
 puts ""
 puts ""
