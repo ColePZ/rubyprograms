4439511b64d232a70eef2350cb2776d90c88e6f5
Cole Pazoid <34737903+ColePZ@users.noreply.github.com>
2018-03-18 13:53:05 +0000

    added beta game

--- Ruby Programs/game.rb
+++ Ruby Programs/game.rb
@@ -1,1 +1,23 @@
-puts "work in progress"
+island = "Mango Island"
+
+puts "A game by HiddenCale"
+puts "WARNING: DO NOT CLOSE THE COMMAND PROMPT AS THERE IS NO SAVE"
+
+puts "Whats your name?"
+player = gets.chomp
+
+puts "Welcome #{player}"
+puts "You were once on a boat with your fellow pirates"
+puts "Your boat crashed somehow into a rock"
+puts "There is an island near to you."
+puts "Your friends are drowning and you escape"
+puts "You found an island! Its called #{island}"
+
+puts "You found a spider! What do you call your new friend!?"
+spider = gets.chomp
+
+puts "Your spider, #{spider} . Points towards a cave!"
+puts "You go into the cave"
+puts ""
+puts ""
+puts "This is only a beta version. So this is the end for now."
